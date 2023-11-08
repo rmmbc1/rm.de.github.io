402134f7fc314aacc8cbf3dfa8630adbfdd753c1
rmmbc1 <rm.mbc1@gmail.com>
2023-11-08 11:37:00 -0600

    commit

---
+++ github_commands_cl.py
@@ -0,0 +1,23 @@
+
+
+# OPEN REPO IN GITHUB, CLICK CODE BUTTON IN GREEN, CLICK HTTP, COPY LINK
+#git remote add origin https://github.com/rmmbc1/rm.de.github.io.git
+#git push -u origin master
+
+
+# GitHub CLI needs to be install for the any of the "gh" commands like "gh repo create" to work
+
+
+
+git init -b main
+
+# to update individual item, 
+git add .
+
+# to update all files in repository, 
+git add --all
+
+
+# "first commit" is comment and optional
+git commit -m "first commit"
+git push
